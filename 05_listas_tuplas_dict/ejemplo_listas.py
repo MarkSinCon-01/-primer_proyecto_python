9162a589ef0f196476243543587cb1e814c062ad
MarkSinCon-01 <sinchecondoymark@gmail.com>
2025-05-16 02:32:12 -0500

    Agregando Listas, tuplas y diccionarios

---
+++ 05_listas_tuplas_dict/ejemplo_listas.py
@@ -0,0 +1,9 @@
+# Listas, tuplas y diccionarios
+
+lista = [1, 2, 3]
+tupla = (4, 5, 6)
+diccionario = {"nombre": "Marco", "edad": 30}
+
+print("Lista:", lista)
+print("Tupla:", tupla)
+print("Diccionario:", diccionario)
